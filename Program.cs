6b150068b1eae1dcf5dfccbe7f90123dfdc19dc9
unknown <legiathinh0508@gmail.com>
2024-08-16 20:47:59 +0700

    SNT

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+        Console.Write("Enter a positive integer: ");
+        number = int.Parse(Console.ReadLine());
+
+        if (number <= 1)
+        {
+            Console.WriteLine("The number is not a prime number.");
+            return;
+        }
+
+        bool isPrime = true;
+        int i = 2;
+        while (i * i <= number)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine("The number is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not a prime number.");
+        }
+    }
+}
